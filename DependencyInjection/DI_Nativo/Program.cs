cea198b7bfa3b8aaef6a18dbe42505806a24ac9a
Natália Souza <nataliasouza.ti@hotmail.com>
2021-09-28 18:10:09 -0300

    DI e IoC com Conteiner - Parte 2

---
+++ DependencyInjection/DI_Nativo/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DI_Nativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceProvider container = RegistrarServices())
+            {
+                var controller = container.GetRequiredService<Controller>();
+                string resultado = controller.Ola("Macoratti");
+                Console.WriteLine(resultado);
+            }
+        }
+        static ServiceProvider RegistrarServices()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<IRecepcaoService, RecepcaoService>();
+            services.AddTransient<Controller>();
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ DependencyInjection/DI_Nativo/RecepcaoService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI_Nativo
+{
+    public class RecepcaoService : IRecepcaoService
+    {
+        public string Saudacao(string nome) => $" Olá, { nome}";
+    }
+}
